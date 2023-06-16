241e885805f0b635f6ad0b107038fc70195394c1
AmitHeshes <amit.heshes@campus.technion.ac.il>
2023-06-16 19:09:14 +0300

    added Song, Duration, FilteredSongIterable, OrederedSongIterable, Genre enum. didn't checked anything

---
+++ src/FilteredSongIterable.java
@@ -0,0 +1,6 @@
+public interface FilteredSongIterable extends Iterable<Song>{
+    public void filterArtist(String artist);
+    public void filterGenre(Song.Genre genre);
+    public void filterDuration(int durationInSeconds);
+
+}
